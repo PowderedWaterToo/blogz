088f97e60564547a51535288ca3242de182398b7
Michael Porter <mb_porter@outlook.com>
2018-04-14 15:04:35 -0500

    posts and new enter on same page

---
+++ main.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(1000))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+#@app.before_request
+#def require_login():
+#    allowed_routes = ['login', 'register']
+#    if request.endpoint not in allowed_routes and 'email' not in session:
+#        return redirect ('/login')
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_entry = Blog(title, body)
+        db.session.add(new_entry)
+        db.session.commit()
+
+    blog = Blog.query.all()
+    return render_template('blog.html', title="Blogs y'all!", blog=blog)
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("logged in")
+            print(session)
+            return redirect('/')
+        else:
+            flash('user info is bunk', 'error')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']  
+        
+        #TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/')
+        else:
+            #TODO - use better message
+            return '<h1>Duplicate user</h1>'
+        
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
